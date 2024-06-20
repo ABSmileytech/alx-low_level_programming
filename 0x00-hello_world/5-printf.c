bc0a350fd17b04f54e1840da124ae69cc12b35ba
Abraham Awoleye <awoleyeabrahamseyi@gmail.com>
2024-06-20 22:37:25 +0100

    C programme

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,7 +4,6 @@
  * Write a C program that prints exactly with proper grammar, but the outcome is a piece of art
  * Return: 0 if exited properly, non-zero otherwise
  */
-
 int main(void)
 {
     printf("with proper grammar, but the outcome is a piece of art,\n");
