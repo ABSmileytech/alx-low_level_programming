9f73f04944b5840e4ab0fea5c550bda932985320
Abraham Awoleye <awoleyeabrahamseyi@gmail.com>
2024-07-01 22:44:33 +0100

    7 Puts

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string, followed by a new line
+ * @str: pointer to the string to be printed
+ */
+void puts_half(char *str)
+{
+	int len = 0, i, n;
+
+	/* Find the length of the string */
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	/* Calculate the starting point for the second half */
+	if (len % 2 == 0)
+	{
+		n = len / 2;
+	}
+	else
+	{
+		n = (len - 1) / 2 + 1;
+	}
+
+	/* Print the second half of the string */
+	for (i = n; i < len; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
